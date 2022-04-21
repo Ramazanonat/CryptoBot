377af5699386bad2a095a99ad1accbce5e98c66c
ramazan <ramazan.onat0@gmail.com>
2022-04-22 01:11:13 +0300

    General Update back version

--- WorkerTown/WorkerTownBot.js
+++ WorkerTown/WorkerTownBot.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
-const kuki = "_ga=GA1.1.1347802490.1649865193; _ga_P9DQW7WC46=GS1.1.1650416115.44.1.1650416144.0; XSRF-TOKEN=eyJpdiI6InFaSEJtTWd1NUtUODZONFFabkNrWUE9PSIsInZhbHVlIjoiZmtMNlRHaDJscFU1K2FOdS9zcldnS1ZWU3E5dXpTaVZwVWhBcGNDVzI0ZFdJa2lVaEhJMXFCOFR3NCswMkFrRW5mM204Y3YySGFsOUs5VjJhbGx0MThoOE5QYTRxR3ZUdXJTaEFNa3pYMkhUdTRwa1pveW8yMXpPeEhJazJkeWwiLCJtYWMiOiIyOTc3MmM1OTc0OWU1MDUyOTE1ZTc2MTVhMWUyOTRjM2IwMWI5MzdiZTQ2YTdmMzJhYTU1YjlhZmYyNTQ3NTRmIiwidGFnIjoiIn0%3D; workertown_session=eyJpdiI6Ii82bmNXUHdFVDg3SHlIWHBXY0ZqMXc9PSIsInZhbHVlIjoiNXNNR3I1a21XQlhLbldqcVBrdWU1NjB6OHg2bjhlb05QMVRXZkFXalpwSUNxeTJQaVljZ09ORjRlNXd6aXh0SjdveFdJVmd1aVVPcno3by8rb3JKY095YUJ1UERsY0h0ZVlSQ2VNYlpTdVBDdHNYVUlWOWZMOFQzMWs0NG5vQVciLCJtYWMiOiI0ZDFiNDVjMzMwMzc0ZjhhNjNlMmQwM2YyMWZkM2ZjZDNjNzAzYjFhOTNkNTM1YzBlMzU3MjhkMTg1YjIwYzUxIiwidGFnIjoiIn0%3D";
-const csrfToken = "MutIwdvOiqv16PqrhFse1xFSjH6m87Bz6E63u29Z";
+const kuki = "_ga=GA1.1.1347802490.1649865193; XSRF-TOKEN=eyJpdiI6Ijd6ZVF4RytIZEx4QVgvS2hXSlZxTnc9PSIsInZhbHVlIjoiWXBJOFR3dCs2cEg0Q0w1QmtHMjdNUnhjckpzUGpHQ3lVSmh2MUhxc2dEOVdIRWxnemc0M2RWdXdTeGI4dGladVozejlveGs5TmhOVkx3cC9nL1RuUXVNZGw5ZUxVK09kUTdjcGVrMTlMWEhqZU5ZaEFiMUszVmdsVTBsVGVheUgiLCJtYWMiOiIzNDc4NDVhODVlYzczMmFjYzAyM2ZmMDA4M2I1NzVkNjdlOWM4YTQzYTU3YWRhYzMxNmVjYjY0Nzc2YWFmNGE5IiwidGFnIjoiIn0=; workertown_session=eyJpdiI6IllEV25GVS9sVURWaHJFY3JpR3hXTEE9PSIsInZhbHVlIjoiS0x1VHBYdTdBVExMLzBRbTFudEx0QzF3Q3dGenN5MUt2M1RXRnd3QlNDZFQrbldUOUlxa00zRnJFeGliMGo5S1FzN0I2SnFxM3BmRkpjS0tRcHg2c1NxKzFXLzN1Mlh0VVVDMDVxQXRiWVlwbjdCY2FVclpVaFQxd1NjQkYvTEwiLCJtYWMiOiIxOTA1NDQyYzIyNjhhYzRmYzc3Zjc0NTg0NmQ3MzYyNTc4ZWEwM2U3ZmMyNmVmMDY2YzZjYzY1YTNjOGM3Y2M2IiwidGFnIjoiIn0=; _ga_P9DQW7WC46=GS1.1.1650572868.51.1.1650576080.0";
+const csrfToken = "WtDW52BzaAsxiVJD7mWeL7rKwSuGIaFKKcwvOEPP";
 
 const customHeader = {
     "accept": "application/json",
@@ -28,65 +28,54 @@ while (true) {
             .then(res => res.json())
             .catch(error => console.error('Error:', error))
             .then(async response => {
-                // console.log(response)
                 for (let worker of response) {
                     let workerId = worker.id;
                     if (worker.resting_until == null && worker.working_until == null) {
                         await postData("https://game.worker.town/api/workers/work", {"worker_id": workerId})
-                        console.log(workerId + " IDLE")
-                    } else if (worker.resting_until != null) {
-                        if (Math.floor(new Date(worker.resting_until).getTime() / 1000) + 10800 <= Math.floor(Date.now() / 1000)) {
-                            await postData("https://game.worker.town/api/workers/rest/callback", {"worker_id": workerId})
-                            console.log(workerId + " GO TO WORK")
-                            await postData("https://game.worker.town/api/workers/work", {"worker_id": "5284"})
-                        } else {
-                            console.log(workerId + " RESTING!")
-                        }
-
-                    } else if (worker.working_until != null) {
-                        if (Math.floor(new Date(worker.working_until).getTime() / 1000) + 10800 <= Math.floor(Date.now() / 1000)) {
-                            await postData("https://game.worker.town/api/workers/work/callback", {"worker_id": workerId})
-                            console.log(workerId + " GO TO RESTING")
-                            await postData("https://game.worker.town/api/workers/rest", {
-                                "worker_id": workerId,
-                                "house_id": 5990,
-                                "slot": 1
-                            })
-                        } else {
-                            console.log(workerId + " WORKING!")
-                        }
-
-                    } else if (worker.working_until != null && Math.floor(new Date(worker.working_until).getTime() / 1000) + 10800 >= Math.floor(Date.now() / 1000)) {
-                        if (Math.floor(new Date(worker.shift.shift_1_time).getTime() / 1000) + 10800 <= Math.floor(Date.now() / 1000) && worker.shift.shift_1_eaten === 0) {
+                        console.log("IDLE" + workerId)
+                    } else if (worker.resting_until != null && Math.floor(new Date(worker.resting_until).getTime() / 1000) <= Math.floor(Date.now() / 1000)) {
+                        await postData("https://game.worker.town/api/workers/rest/callback", {"worker_id": workerId})
+                        console.log("GO TO WORK" + workerId)
+                        await postData("https://game.worker.town/api/workers/work", {"worker_id": workerId})
+                    } else if (worker.working_until != null && Math.floor(new Date(worker.working_until).getTime() / 1000) <= Math.floor(Date.now() / 1000)) {
+                        await postData("https://game.worker.town/api/workers/work/callback", {"worker_id": workerId})
+                        console.log("GO TO RESTING" + workerId)
+                        await postData("https://game.worker.town/api/workers/rest", {
+                            "worker_id": workerId,
+                            "house_id": 5990,
+                            "slot": 1
+                        })
+                    } else if (worker.working_until != null && Math.floor(new Date(worker.working_until).getTime() / 1000) >= Math.floor(Date.now() / 1000)) {
+                        if (Math.floor(new Date(worker.shift.shift_1_time).getTime() / 1000) <= Math.floor(Date.now() / 1000) && worker.shift.shift_1_eaten === 0) {
                             await postData('https://game.worker.town/api/workers/feed', {
                                 "worker_id": workerId,
                                 "shift": 1
                             })
-                            console.log(workerId, +" SHIFT 1")
-                        } else if (Math.floor(new Date(worker.shift.shift_2_time).getTime() / 1000) + 10800 <= Math.floor(Date.now() / 1000) && worker.shift.shift_2_eaten === 0) {
+                            console.log("SHIFT 1 |" + workerId)
+                        } else if (Math.floor(new Date(worker.shift.shift_2_time).getTime() / 1000) <= Math.floor(Date.now() / 1000) && worker.shift.shift_2_eaten === 0) {
                             await postData('https://game.worker.town/api/workers/feed', {
                                 "worker_id": workerId,
                                 "shift": 2
                             })
-                            console.log(workerId + " SHIFT 2")
-                        } else if (Math.floor(new Date(worker.shift.shift_3_time).getTime() / 1000) + 10800 <= Math.floor(Date.now() / 1000) && worker.shift.shift_3_eaten === 0) {
+                            console.log("SHIFT 2 |" + workerId)
+                        } else if (Math.floor(new Date(worker.shift.shift_3_time).getTime() / 1000) <= Math.floor(Date.now() / 1000) && worker.shift.shift_3_eaten === 0) {
                             await postData('https://game.worker.town/api/workers/feed', {
                                 "worker_id": workerId,
                                 "shift": 3
                             })
-                            console.log(workerId + " SHIFT 3")
-                        } else if (Math.floor(new Date(worker.shift.shift_4_time).getTime() / 1000) + 10800 <= Math.floor(Date.now() / 1000) && worker.shift.shift_4_eaten === 0) {
+                            console.log("SHIFT 3 |" + workerId)
+                        } else if (Math.floor(new Date(worker.shift.shift_4_time).getTime() / 1000) <= Math.floor(Date.now() / 1000) && worker.shift.shift_4_eaten === 0) {
                             await postData('https://game.worker.town/api/workers/feed', {
                                 "worker_id": workerId,
                                 "shift": 4
                             })
-                            console.log(workerId + " SHIFT 4")
+                            console.log("SHIFT 4 |" + workerId)
                         }
                     }
                 }
             });
         console.log("WAITING 10 MINUTES | CHECKED TIME => " + new Date().toLocaleString("tr-TR", {timeZone: 'Europe/Istanbul'}));
-        await sleep(1000 * 60 * 10);
+        await sleep(1000 * 60 * 15);
     } catch (e) {
         console.log(e);
     }
